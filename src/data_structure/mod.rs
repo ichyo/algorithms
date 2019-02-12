c3cc3f9f53e08ff3bf503cdcc8a6684606ed342a
ichyo <ichyo00@gmail.com>
2019-02-12 12:17:47 +0900

    Add doc comments

--- src/data_structure/mod.rs
+++ src/data_structure/mod.rs
@@ -1,5 +1,11 @@
+//! data structures to process some type of queries efficiently
+//!
+//! Now, it contains these data structures
+//! * BIT: [`BIT`], [`BITRange`]
+//! * UnionFind: [`UnionFind`]
+//!
 mod bit;
 mod union_find;
 
-pub use self::bit::*;
-pub use self::union_find::*;
+pub use self::bit::{BITRange, BIT};
+pub use self::union_find::UnionFind;
