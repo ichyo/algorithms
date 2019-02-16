f87bd613480f9863a83923a7ca966323986c8db5
ichyo <ichyo00@gmail.com>
2019-02-16 12:02:04 +0900

    Add xorshift

--- src/util/mod.rs
+++ src/util/mod.rs
@@ -1,7 +1,10 @@
 //! util functions and trails to help typical implementation
 //!
 //! * [`Permutation`](trait.Permutation.html) - Add `next_permutation`, `prev_permutation` for slices.
+//! * [`XorShift`](struct.XorShift.html) - Provide very simple random function.
 //!
 mod permutation;
+mod random;
 
-pub use self::permutation::*;
+pub use self::permutation::Permutation;
+pub use self::random::XorShift;
---
+++ src/util/random.rs
@@ -0,0 +1,53 @@
+use std::num::Wrapping;
+
+/// Not secure but fast random generator.
+///
+/// # Examples
+/// ```
+/// use algonium::util::XorShift;
+/// let mut xorshift = XorShift::new();
+/// assert_eq!(3701687786, xorshift.next_u32());
+/// assert_eq!(458299110, xorshift.next_u32());
+/// ```
+#[derive(Clone, Debug, Default)]
+pub struct XorShift {
+    x: Wrapping<u32>,
+    y: Wrapping<u32>,
+    z: Wrapping<u32>,
+    w: Wrapping<u32>,
+}
+
+impl XorShift {
+    /// Create object with default seed.
+    pub fn new() -> XorShift {
+        XorShift {
+            x: Wrapping(123_456_789),
+            y: Wrapping(362_436_069),
+            z: Wrapping(521_288_629),
+            w: Wrapping(88_675_123),
+        }
+    }
+
+    /// Get next random u32 number.
+    pub fn next_u32(&mut self) -> u32 {
+        let x = self.x;
+        let t = x ^ (x << 11);
+        self.x = self.y;
+        self.y = self.z;
+        self.z = self.w;
+        let v = self.w;
+        self.w = v ^ (v >> 19) ^ (t ^ (t >> 8));
+        self.w.0
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::XorShift;
+    #[test]
+    fn test_basic() {
+        let mut xorshift = XorShift::new();
+        assert_eq!(3701687786, xorshift.next_u32());
+        assert_eq!(458299110, xorshift.next_u32());
+    }
+}
