e460eec40157193a838a05d50ad9db84d568ffad
ichyo <ichyo00@gmail.com>
2022-11-06 15:46:01 +0900

    Remove all system-tests

--- system-test/target/debug/deps/text_scanner-f2cb046b519a3d68.d
+++
@@ -1,5 +0,0 @@
-/home/shoichi/dev/src/github.com/ichyo/algorithms/system-test/target/debug/deps/text_scanner-f2cb046b519a3d68.rmeta: /home/shoichi/.cargo/git/checkouts/rust-text-scanner-9018c83515ef99ef/ddfb683/src/lib.rs
-
-/home/shoichi/dev/src/github.com/ichyo/algorithms/system-test/target/debug/deps/text_scanner-f2cb046b519a3d68.d: /home/shoichi/.cargo/git/checkouts/rust-text-scanner-9018c83515ef99ef/ddfb683/src/lib.rs
-
-/home/shoichi/.cargo/git/checkouts/rust-text-scanner-9018c83515ef99ef/ddfb683/src/lib.rs:
