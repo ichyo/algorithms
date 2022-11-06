d5768a108a5db30ad0d620b11a4233566b483987
ichyo <ichyo00@gmail.com>
2022-11-06 15:44:32 +0900

    Remove verify

---
+++ system-test/target/debug/deps/unionfind-2a25361d9f699fca.d
@@ -0,0 +1,5 @@
+/home/shoichi/dev/src/github.com/ichyo/algorithms/system-test/target/debug/deps/unionfind-2a25361d9f699fca.rmeta: src/bin/unionfind.rs
+
+/home/shoichi/dev/src/github.com/ichyo/algorithms/system-test/target/debug/deps/unionfind-2a25361d9f699fca.d: src/bin/unionfind.rs
+
+src/bin/unionfind.rs:
---
+++ system-test/target/debug/deps/unionfind-92623b67ab70454f.d
@@ -0,0 +1,5 @@
+/home/shoichi/dev/src/github.com/ichyo/algorithms/system-test/target/debug/deps/unionfind-92623b67ab70454f.rmeta: src/bin/unionfind.rs
+
+/home/shoichi/dev/src/github.com/ichyo/algorithms/system-test/target/debug/deps/unionfind-92623b67ab70454f.d: src/bin/unionfind.rs
+
+src/bin/unionfind.rs:
